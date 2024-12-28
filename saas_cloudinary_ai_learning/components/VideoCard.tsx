088ea588d23b5fa4e41411654363ac330e1ebf1b
Ujjawal Kumar <ujjawalsingh800007@gmail.com>
2024-12-28 20:00:39 +0530

    SAAS_Cloudinary Integrated DaisyUI for responsive UI component

--- saas_cloudinary_ai_learning/components/VideoCard.tsx
+++ saas_cloudinary_ai_learning/components/VideoCard.tsx
@@ -1,10 +1,11 @@
-import React, {useState, useEffect, useCallback} from 'react'
-import {getCldImageUrl, getCldVideoUrl} from "next-cloudinary"
-import { Download, Clock, FileDown, FileUp } from "lucide-react";
 import dayjs from 'dayjs';
-import realtiveTime from "dayjs/plugin/relativeTime"
-import {filesize} from "filesize"
-import { Video } from '@prisma/client';
+import {filesize} from "filesize";
+import React, {useState, useEffect, useCallback} from 'react';
+import {getCldImageUrl, getCldVideoUrl} from "next-cloudinary";
+import { Download, Clock, FileDown, FileUp } from "lucide-react";
+
+import { Video } from '@/types';
+import realtiveTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(realtiveTime)
 
